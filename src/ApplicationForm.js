216939d5474ee4775dc11328e8cdcf13cda0b153
trong nhan nguyen <ntnhan30@gmail.com>
2018-10-18 11:53:31 +0200

    input stored as states in ApplicationForm

--- src/ApplicationForm.js
+++ src/ApplicationForm.js
@@ -11,9 +11,20 @@ export default class ApplicationForm extends React.Component {
       salary: ""
     };
   }
+
+  handleInputChange(stateFieldName, event) {
+    let newState = {};
+    newState[stateFieldName] = event.target.value;
+    this.setState(newState);
+  }
+
   handleClick(e) {
     e.preventDefault();
     console.log(e.target.value);
+    console.log(this.state.fullName);
+    console.log(this.state.email);
+    console.log(this.state.phoneNumber);
+    console.log(this.state.salary);
     console.log("THIS IS TRIGGERED");
   }
   render() {
@@ -23,27 +34,30 @@ export default class ApplicationForm extends React.Component {
           <Label>Full Name</Label>
           <Input
             type="text"
-            name="fullName"
-            //id="exampleEmail"
-            //  placeholder="with a placeholder"
+            value={this.state.fullName}
+            onChange={e => {
+              this.handleInputChange("fullName", e);
+            }}
           />
         </FormGroup>
         <FormGroup>
           <Label>Email</Label>
           <Input
             type="email"
-            name="email"
-            //id="examplePassword"
-            //  placeholder="password placeholder"
+            value={this.state.email}
+            onChange={e => {
+              this.handleInputChange("email", e);
+            }}
           />
         </FormGroup>
         <FormGroup>
           <Label>Phone Number</Label>
           <Input
             type="text"
-            name="Phone Number"
-            //id="examplePassword"
-            //  placeholder="password placeholder"
+            value={this.state.phoneNumber}
+            onChange={e => {
+              this.handleInputChange("phoneNumber", e);
+            }}
           />
         </FormGroup>
 
@@ -51,27 +65,67 @@ export default class ApplicationForm extends React.Component {
           <legend>Salary</legend>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="radio1" /> 0 - 1.000 €
+              <Input
+                type="radio"
+                value="0 - 1.000 €"
+                checked={this.state.salary === "0 - 1.000 €"}
+                onChange={e => {
+                  this.handleInputChange("salary", e);
+                }}
+              />{" "}
+              0 - 1.000 €
             </Label>
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="radio1" /> 1.000 - 2.000 €
+              <Input
+                type="radio"
+                value="1.000 - 2.000 €"
+                checked={this.state.salary === "1.000 - 2.000 €"}
+                onChange={e => {
+                  this.handleInputChange("salary", e);
+                }}
+              />{" "}
+              1.000 - 2.000 €
             </Label>
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="radio1" /> 2.000 - 3.000 €
+              <Input
+                type="radio"
+                value="2.000 - 3.000 €"
+                checked={this.state.salary === "2.000 - 3.000 €"}
+                onChange={e => {
+                  this.handleInputChange("salary", e);
+                }}
+              />{" "}
+              2.000 - 3.000 €
             </Label>
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="radio1" /> 3.000 - 4.000 €
+              <Input
+                type="radio"
+                value="3.000 - 4.000 €"
+                checked={this.state.salary === "3.000 - 4.000 €"}
+                onChange={e => {
+                  this.handleInputChange("salary", e);
+                }}
+              />{" "}
+              3.000 - 4.000 €
             </Label>
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="radio" name="radio1" /> Mehr als 4.000 €
+              <Input
+                type="radio"
+                value="Mehr als 4.000 €"
+                checked={this.state.salary === "Mehr als 4.000 €"}
+                onChange={e => {
+                  this.handleInputChange("salary", e);
+                }}
+              />{" "}
+              Mehr als 4.000 €
             </Label>
           </FormGroup>
         </FormGroup>
